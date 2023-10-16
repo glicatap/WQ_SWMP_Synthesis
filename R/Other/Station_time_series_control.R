c2e686613fbee631ce39b90df6047cdd1e2bc192
Kim Cressman <kim@catbirdstats.com>
2023-10-16 14:45:44 -0500

    more reorg + station time series

---
+++ R/Other/Station_time_series_control.R
@@ -0,0 +1,33 @@
+#' This script is the control board for generating files for each reserve
+#' that include time series for each parameter at each station
+library(foreach)
+library(doParallel)
+
+reserves <- c("ace", "apa", "cbm", "cbv", "del", "elk", "gnd", "grb", "gtm", 
+              "hee", "hud", "jac", "job", "kac", "lks", "mar", "nar", "niw", 
+              "noc", "owc", "pdb", "rkb", "sap", "sfb", "sos", "tjr", "wel", 
+              "wkb", "wqb")
+
+# setup parallel backend
+# cl<-makeCluster(6)  
+# registerDoParallel(cl)
+strt<-Sys.time()
+
+# process all stations
+foreach(res = reserves) %do% {
+  outpath <- here::here("Outputs", "time_series_graphs")
+  outname <- paste0(toupper(res), "_TimeSeries_", Sys.Date(), ".html")
+
+  try(
+    xfun::Rscript_call(
+      rmarkdown::render,
+      list(input = here::here("R", "Other", "Station_time_series.Rmd"), 
+           params = list("reserve" = res),
+           output_file = here::here(outpath, outname))
+    )
+  )
+}
+
+Sys.time() - strt
+# stopCluster(cl)
+beepr::beep(8)
