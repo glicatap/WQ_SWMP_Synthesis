bdd59c1f9c79648e204227a920c8948cb826690f
Kim Cressman <kim@catbirdstats.com>
2024-06-27 13:06:48 -0500

    verifying some things

--- R/initial_explorations/met_prcp.R
+++ R/initial_explorations/met_prcp.R
@@ -1,3 +1,25 @@
+#' this script was originally written in Dec. 2023 to determine which
+#' transformation of precip would work best in the gams
+#'
+#' re-visiting in June 2024 because natural-log would yield more interpretable
+#' results than square-root
+#' so even though square-root worked best, i want to make sure it's
+#' "better enough" than log to be worth the loss of interpretability
+#' ugh, I think it is. I don't like the QQ plot or histogram of residuals
+#' for log as much as for sqrt. response vs. fitted looks better for sqrt too.
+#' 
+#' looks like I had been working off of data frames from another script;
+#' adding enough to make this one standalone
+
+
+library(tidyverse)
+library(mgcv)
+library(gratia)
+
+source(here::here("helper_files", "definitions.R"))
+source(here::here("helper_files", "functions.R"))
+load(here::here("Data", "QAQCd_monthly_byType", "SWMP_monthlyMET.RData"))
+
 stns_met_d10
 # sqrt worked best for all the ones I've checked:
 # acebp, apaeb, cbmjb, gndcr, niwol
@@ -8,7 +30,7 @@ i = 16
 dat <- subset_df("wq", met, stns[i], "totprcp_total")
 
 dat2 <- dat
-dat2$value <- log10(dat$value + 1)
+dat2$value <- log(dat$value + 1)
 
 dat3 <- dat
 dat3$value <- sqrt(dat$value)
